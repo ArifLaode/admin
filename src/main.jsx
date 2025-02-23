ea916fd978c884ec544aba1a3a60b360889b8fdc
ArifLaode <muhamadariflaode99@gmail.com>
2025-02-23 14:23:07 +0800

    modified:   package.json 	modified:   src/main.jsx 	modified:   yarn.lock

--- src/main.jsx
+++ src/main.jsx
@@ -4,26 +4,34 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login.jsx';
+import { ClipLoader } from 'react-spinners';
 
 const root = createRoot(document.getElementById('root'));
 
 function AppWrapper() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsLoggedIn(true);
-      console.log(token);
-    }
-  },);
+    const checkAuth = () => {
+      const token = localStorage.getItem('token');
+      setIsLoggedIn(!!token);
+      setIsLoading(false);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isLoading) {
+    return <ClipLoader />;
+  }
 
   return (
     <StrictMode>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login onLoginSuccess={() => setIsLoggedIn(true)} />} />
-          <Route path="/*" element={isLoggedIn? <App />: <Navigate to="/login" />} />
+          <Route path="/*" element={isLoggedIn ? <App /> : <Navigate to="/login" />} />
         </Routes>
       </BrowserRouter>
     </StrictMode>
