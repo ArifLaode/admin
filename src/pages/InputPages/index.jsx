a0f1d6c35b3a6f28fdb0f120b9515e8a20d1183f
ArifLaode <muhamadariflaode99@gmail.com>
2025-03-04 05:15:05 +0800

    modified:   src/pages/InputPages/components/TableCell.jsx 	modified:   src/pages/InputPages/index.jsx

--- src/pages/InputPages/index.jsx
+++ src/pages/InputPages/index.jsx
@@ -109,31 +109,7 @@ const InputPage = () => {
         }
       >
         <div className="table-container">
-          <table className="data-table" style={{ width: "100%" }}>
-            <thead>
-              <tr>
-                {column.map((col) => (
-                  <th key={col.accessor} className="table-header">
-                    {col.Header}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((item, index) => (
-                <tr
-                  key={item.resi}
-                  className={index % 2 === 0 ? "even" : "odd"}
-                >
-                  {column.map((col) => (
-                    <td key={col.accessor} className="table-cell">
-                      {item[col.accessor]}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          
         </div>
         <Modal
           isOpen={modalOpen}
